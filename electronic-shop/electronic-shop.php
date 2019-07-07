25e816854fc71220db2e33d5fe6d8bcd7f320409
Roberto Peruzzo <roberto.peruzzo@studioaqua.it>
2019-07-07 15:18:49 +0200

    Optimized algorithm sorting the input arrays.

--- electronic-shop/electronic-shop.php
+++ electronic-shop/electronic-shop.php
@@ -4,56 +4,41 @@
  * Complete the getMoneySpent function below.
  */
 function getMoneySpent($keyboards, $drives, $b, $n, $m) {
-    $sum = [];
+    $cost = -1;
+    rsort($keyboards);
+    sort($drives);
+
     for ($i = 0; $i < $n; $i++) {
         if ($keyboards[$i] < $b) {
-            for ($k = $m - 1; $k >= 0; $k--) {
+            for ($k = 0; $k < $m; $k++) {
                 $ammount = $keyboards[$i] + $drives[$k];
                 if ($ammount <= $b) {
-                    $sum[] = $ammount;
+                    $cost = max($ammount, $cost);
+                }
+                else {
+                    break;
                 }
             }
         }
     }
-
-    if (!empty($sum)) {
-        sort($sum);
-        return end($sum);
-    }
-    else {
-        return -1;
-    }
-
+    return $cost;
 }
 
-$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+$line = trim(fgets(STDIN));
+list($b, $n, $m) = array_map('intval', explode(' ', $line));
 
-$stdin = fopen("php://stdin", "r");
+$line = trim(fgets(STDIN));
+$keyboards = array_map('intval', explode(' ', $line));
 
-fscanf($stdin, "%[^\n]", $bnm_temp);
-$bnm = explode(' ', $bnm_temp);
-
-$b = intval($bnm[0]);
-
-$n = intval($bnm[1]);
-
-$m = intval($bnm[2]);
-
-fscanf($stdin, "%[^\n]", $keyboards_temp);
-
-$keyboards = array_map('intval', preg_split('/ /', $keyboards_temp, -1, PREG_SPLIT_NO_EMPTY));
-
-fscanf($stdin, "%[^\n]", $drives_temp);
-
-$drives = array_map('intval', preg_split('/ /', $drives_temp, -1, PREG_SPLIT_NO_EMPTY));
+$line = trim(fgets(STDIN));
+$drives = array_map('intval', explode(' ', $line));
 
 /*
- * The maximum amount of money she can spend on a keyboard and USB drive, or -1 if she can't purchase both items
+ * The maximum amount of money she can spend on a keyboard and USB drive,
+ * or -1 if she can't purchase both items
  */
-
 $moneySpent = getMoneySpent($keyboards, $drives, $b, $n, $m);
 
-fwrite($fptr, $moneySpent . "\n");
-
-fclose($stdin);
-fclose($fptr);
+$stdout = fopen('php://stdout', 'w');
+fprintf($stdout, $moneySpent);
+fclose($stdout);
